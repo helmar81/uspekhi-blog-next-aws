e2f65177521fae0b81a9db2f21f5a8e91941d269
Helmar Bachle <42181669+helmar81@users.noreply.github.com>
2024-04-07 09:13:07 +0200

    Update _app.tsx
    added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,17 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/main.css";
 
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+
+          <GoogleAnalytics trackPageViews />
+
+<GoogleAnalytics gtagUrl="https://www.googletagmanager.com/gtag/js?id=G-GMBT9699CL/gtag.js"/>
+        
         <link
           rel="alternate"
           type="application/rss+xml"
